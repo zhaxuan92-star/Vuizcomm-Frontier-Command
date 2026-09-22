eea14cc7ac1a6686fa8ee0a397f023fa2ed098fa
zhaxuan92-star <zhaxuan92@gmail.com>
2026-09-22 12:09:28 +0700

    Add main activity

--- app/src/main/java/com/vuizcomm/frontier/MainActivity.java
+++ app/src/main/java/com/vuizcomm/frontier/MainActivity.java
@@ -1,24 +1,18 @@
 package com.vuizcomm.frontier;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
-
 public class MainActivity extends Activity {
     private FrontierView game;
-
     @Override public void onCreate(Bundle state) {
         super.onCreate(state);
         game = new FrontierView(this);
         setContentView(game);
     }
-
     @Override protected void onPause() {
         super.onPause();
-        game.save();
+        if (game != null) game.save();
     }
-
     void notice(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
